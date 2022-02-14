95b4ded88f3956563544b5295eab2fcab05bc990
nysteo <teonys@nyu.edu>
2022-02-14 15:30:20 -0500

    ADD Basic Layout Component

---
+++ components/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default function Layout({children}){
+  return (
+    <div className="bg-gray-900">
+      <Head>
+        <title>teo</title>
+        <meta name="description" content="my personal website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex flex-col justify-center px-8 bg-gray-900">
+        {children}
+      </main>
+    </div>
+  );
+};
