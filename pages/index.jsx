45b133dfb8b24ebd3b9859a35d8f4713685f08af
nysteo <teonys@nyu.edu>
2022-02-14 15:37:29 -0500

    ADD Basic Hero

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,66 +1,32 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import React from 'react';
+
+import Layout from '../components/Layout';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+    <Layout>
+      <div className="flex flex-col justify-center items-start max-w-2xl border-gray-200 mx-auto pb-16">
+        <div className="flex flex-col-reverse sm:flex-row items-start">
+          <div className="flex flex-col pr-8">
+            <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-1 text-black dark:text-white">
+              Teo Nys 
+            </h1>
+            <h2 className="text-gray-700 dark:text-gray-200 mb-4">
+              Computer Scientist at 
+              <span className="font-semibold"> NYU</span>
+              , Software Engineer at
+              <span className="font-semibold"> EPAM</span>
+            </h2>
+            <p className="text-gray-600 dark:text-gray-400 mb-16">
+              Helping developers build a faster web. Teaching about web
+              development, serverless, and React / Next.js.
             </p>
-          </a>
+          </div>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
+        <h3 className="font-bold text-xl md:text-4xl tracking-tight mb-6 text-black dark:text-white">
+          Featured Projects
+        </h3>
+      </div>
+    </Layout>
   )
 }
